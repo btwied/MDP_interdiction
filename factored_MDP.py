7d2257e7a941e00d086405c43e6e14676c249239
Bryce Wiedenbeck <btwied@gmail.com>
2013-06-13 12:07:22 -0700

    basic implementation and comments

--- factored_MDP.py
+++ factored_MDP.py
@@ -1,52 +1,33 @@
+from collections import namedtuple
 
+from HashableClasses import h_dict
 
 class MDP:
 	"""
 	TODO: comment this!
 	"""
-	def __init__(self, variables, actions, rewards, basis):
+	def __init__(self, variables, actions, rewards):
 		"""
-		TODO: comment this!
-		"""
-		#TODO: implement this!
-		pass
+		variables: A collection of Variable objects.
+		actions: A collection of Action objects. a.prereqs and a.outcomes
+				should only include v's from variables
+		rewards: A mapping of v's in variables to floats.
 
-	#TODO: implement this!
-
-
-class MDP_variable:
-	"""
-	TODO: comment this!
-	"""
-	def __init__(self, name, init_value=False, domain=[True,False]):
+		may also need to specify a basis
 		"""
-		TODO: comment this!
-		"""
-		self.name = name
-		self.init_value = init_value
-		self.domain = domain
-		#TODO: finish implementing this!
+		self.variables = variables
+		self.actions = actions
+		self.rewards = rewards
 
 	#TODO: finish implementing this!
 
+# value must be an element of domain
+Variable = namedtuple("Variable", "name value domain")
 
-class MDP_action:
-	"""
-	TODO: comment this!
-	"""
-	def __init__(self, name, prereqs, outcomes, probs, cost):
-		"""
-		TODO: comment this!
-		"""
-		self.name = name
-		self.prereqs = prereqs
-		self.outcomes = outcomes
-		self.probs = probs
-		self.cost = cost
-		#TODO: finish implementing this!
-		pass
+# prereqs should be an h_dict mapping vars to vals
+# outcomes should be an h_dict mapping vars to probs
+Action = namedtuple("Action", "name prereqs outcomes cost")
 
-	#TODO: finish implementing this!
 
 
 if __name__ == "__main__":
