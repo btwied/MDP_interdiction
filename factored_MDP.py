fec9ad329bd5c1bff7079ecd0194ed5095d778a1
Bryce Wiedenbeck <btwiede@sandia.gov>
2013-06-14 11:32:59 -0700

    Vectorized states/variables.

--- factored_MDP.py
+++ factored_MDP.py
@@ -1,34 +1,76 @@
 from collections import namedtuple
+from random import randrange, sample
 
-from HashableClasses import h_dict
+from numpy.random import uniform
+from numpy import zeros
+
+from warnings import warn
+try:
+	import gurobipy
+except ImportError as e:
+	warn(e.message)
 
 class MDP:
 	"""
+	Stores a restricted factored MDP and allows conversion to gurobi LP.
+
+	Restrictions:
+		- boolean variables
+		- actions have prerequisites
+		- actions induce distributions over fixed outcomes
+		- an outcome can add some variables and/or remove others
+
 	TODO: comment this!
 	"""
-	def __init__(self, variables, actions, rewards):
+	def __init__(self, variables, initial, actions, rewards, basis):
 		"""
-		variables: A collection of Variable objects.
-		actions: A collection of Action objects. a.prereqs and a.outcomes
-				should only include v's from variables
-		rewards: A mapping of v's in variables to floats.
+		variables: a collection of variable names
+		initial: the subset of variables that are initially True
+		actions: 
+		rewards: 
+		basis: 
 
 		may also need to specify a basis
 		"""
-		self.variables = variables
+		self.variables = sorted(set(variables))
+		self.variable_index = {v:i for i,v in enumerate(self.variables)}
+		self.initial = zeros(len(self.variables), dtype=bool)
+		self.initial[[self.variable_index[v] for v in initial]] += True
 		self.actions = actions
 		self.rewards = rewards
+		self.basis = basis
 
-	#TODO: finish implementing this!
+	#TODO: convert the MDP to a linear program
 
-# value must be an element of domain
-Variable = namedtuple("Variable", "name value domain")
-
-# prereqs should be an h_dict mapping vars to vals
-# outcomes should be an h_dict mapping vars to probs
+# prereqs should be a dict mapping vars to vals
+# outcomes should be a dict mapping tuples of vars to probs
+# 0 < probs; sum(probs) < 1
 Action = namedtuple("Action", "name prereqs outcomes cost")
 
 
+def random_MDP(min_vars=10, max_vars=10, min_acts=10, max_acts=10, \
+				max_prereqs=2, min_outs=10, max_outs=10, max_outs_per_act=3, \
+				max_vars_per_out=3, min_cost=0, max_cost=10, min_discount=.8, \
+				max_discount=.999, nonzero_rwd_prob=0.3, min_rwd=-10, \
+				max_rwd=10):
+	"""Creates an MDP for testing."""
+	MDP_vars = ['v'+str(i) for i in range(randrange(min_vars, max_vars+1))]
+	MDP_outs = [tuple(sample(MDP_vars, randrange(1, max_vars_per_out+1))) for \
+				i in range(randrange(min_outs, max_outs+1))]
+
+	MDP_acts = []
+	for i in range(randrange(min_acts, max_acts+1)):
+		act_prereqs = {v:True for v in sample(MDP_vars, \
+						randrange(max_prereqs+1))}
+		act_outs =  sample(MDP_outs, randrange(1, max_outs_per_act+1))
+		act_probs = uniform(0,1,10)
+		act_probs /= (act_probs.sum() / uniform(min_discount, max_discount))
+		MDP_acts.append(Action("a"+str(i), act_prereqs, dict(zip(act_outs, \
+						act_probs)), uniform(min_cost, max_cost)))
+
+	MDP_rwds = {v : uniform(min_rwd, max_rwd) for v in filter(lambda x: \
+				uniform(0,1) < nonzero_rwd_prob, MDP_vars)}
+	return MDP(MDP_vars, MDP_acts, MDP_rwds)
 
 if __name__ == "__main__":
 	pass
